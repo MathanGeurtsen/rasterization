cef79dd0140dee2541035a91f4be9d2a13de26f4
Bart Rademaker <b.j.rademaker@students.uu.nl>
2017-06-27 15:09:06 +0200

    shader werd niet geadd

---
+++ shaders/skyvs.glsl
@@ -0,0 +1,12 @@
+﻿#version 330 core
+in vec3 vPosition;
+
+out vec3 TexCoords;
+
+uniform mat4 transform;
+
+void main()
+{
+    TexCoords = vPosition;
+    gl_Position = transform * vec4(vPosition, 1.0);
+}  
